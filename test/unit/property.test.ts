d62d204b54f209b2d6b2ad83388f02afc7fe822d
lemengbin <lemengbin@163.com>
2023-08-09 19:02:19 +0800

    rename test file

--- test/unit/property.test.ts
+++ test/unit/property.test.ts
@@ -1,13 +1,13 @@
-import {Safe4Plugin} from "../src";
 import {Web3} from "web3";
-import {expect} from "chai";
+import {Safe4Plugin} from "../../src";
 
 describe('ChainlinkPlugin Tests', () => {
     test("getPropertyValue", async function () {
         const web3 = new Web3("http://127.0.0.1:8545");
         web3.registerPlugin(new Safe4Plugin());
         const result = await web3.safe4.getPropertyValue();
-        console.log("=============" + result);
-        expect(result).equal("hello");
+        // eslint-disable-next-line no-console
+        console.log(`=============${result}`);
+        expect(result).toBe("hello");
     });
 });
