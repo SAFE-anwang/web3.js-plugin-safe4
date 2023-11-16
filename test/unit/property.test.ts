29b59057ece2ff3f6e505ff544d3a99baaeb2a78
lemengbin <lemengbin@163.com>
2023-11-16 20:19:03 +0800

    update unit test for property

--- test/unit/property.test.ts
+++ test/unit/property.test.ts
@@ -13,48 +13,48 @@ describe('Safe4Plugin Property Tests', () => {
 
     test("add", async () => {
         await web3.eth.personal.unlockAccount(accounts[0], '123', 1000);
-        const result = await web3.safe4.property.add(accounts[0], "test_name2", 2, "test name2");
+        const result = await web3.safe4.sysproperty.add(accounts[0], "test_name2", 2, "test name2");
         console.log(result);
         await web3.eth.personal.lockAccount(accounts[0]);
     });
 
     test("applyUpdate", async () => {
         await web3.eth.personal.unlockAccount(accounts[3], '123', 10);
-        const result = await web3.safe4.property.applyUpdate(accounts[2], "block_space", 10, "update block space");
+        const result = await web3.safe4.sysproperty.applyUpdate(accounts[2], "block_space", 10, "update block space");
         console.log(result);
         await web3.eth.personal.lockAccount(accounts[3]);
     });
 
     test("vote4Update", async () => {
         await web3.eth.personal.unlockAccount(accounts[4], '123', 10);
-        const result = await web3.safe4.property.vote4Update(accounts[3], "block_space", 1);
+        const result = await web3.safe4.sysproperty.vote4Update(accounts[3], "block_space", 1);
         console.log(result);
         await web3.eth.personal.lockAccount(accounts[4]);
     });
 
     test("getInfo", async () => {
-        const result = await web3.safe4.property.getInfo("block_space");
+        const result = await web3.safe4.sysproperty.getInfo("block_space");
         console.log(result);
     });
 
     test("getUnconfirmedInfo", async () => {
-        const result = await web3.safe4.property.getUnconfirmedInfo("block_space");
+        const result = await web3.safe4.sysproperty.getUnconfirmedInfo("block_space");
         console.log(result);
     });
 
     test("getValue", async () => {
-        const result = await web3.safe4.property.getValue("block_space");
+        const result = await web3.safe4.sysproperty.getValue("block_space");
         console.log(result);
         expect(Number(result)).toBe(30);
     });
 
     test("getAll", async () => {
-        const result = await web3.safe4.property.getAll();
+        const result = await web3.safe4.sysproperty.getAll();
         console.log(result);
     });
 
     test("getAllUnconfirmed", async () => {
-        const result = await web3.safe4.property.getAllUnconfirmed();
+        const result = await web3.safe4.sysproperty.getAllUnconfirmed();
         console.log(result);
     });
 });
