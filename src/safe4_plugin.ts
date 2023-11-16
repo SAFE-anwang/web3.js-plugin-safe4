2439f0b15ed75fb925afa6fb741b8649946e38aa
lemengbin <lemengbin@163.com>
2023-11-16 20:18:23 +0800

    init contract object for plugin

--- src/safe4_plugin.ts
+++ src/safe4_plugin.ts
@@ -1,18 +1,38 @@
 import {Web3Context, Web3PluginBase} from 'web3';
 import {Property} from "./contracts/property";
+import {AccountManager} from "./contracts/accountmanager";
+import {MasterNode} from "./contracts/masternode";
+import {SuperNode} from "./contracts/supernode";
+import {SNVote} from "./contracts/snvote";
+import {Proposal} from "./contracts/proposal";
 
 export class Safe4Plugin extends Web3PluginBase {
     public pluginNamespace = "safe4";
-    public property: Property
+    public sysproperty: Property;
+    public account: AccountManager;
+    public masternode: MasterNode;
+    public supernode: SuperNode;
+    public snvote: SNVote;
+    public proposal: Proposal;
 
     public constructor() {
         super();
-        this.property = new Property();
+        this.sysproperty = new Property();
+        this.account = new AccountManager();
+        this.masternode = new MasterNode();
+        this.supernode = new SuperNode();
+        this.snvote = new SNVote();
+        this.proposal = new Proposal();
     }
 
     public link(parentContext: Web3Context) {
         super.link(parentContext);
-        this.property.link(parentContext);
+        this.sysproperty.link(parentContext);
+        this.account.link(parentContext);
+        this.masternode.link(parentContext);
+        this.supernode.link(parentContext);
+        this.snvote.link(parentContext);
+        this.proposal.link(parentContext);
     }
 }
 
