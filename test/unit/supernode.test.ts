e8d46c0560be7f463459ca13d6623802b56f2db9
lemengbin <lemengbin@163.com>
2023-12-13 21:14:33 +0800

    update SuperNode-Test

--- test/unit/supernode.test.ts
+++ test/unit/supernode.test.ts
@@ -75,7 +75,7 @@ describe('Safe4Plugin SuperNode Tests', () => {
 
     test("turnRegister", async () => {
         let fromAddr = "0x044f9c93b57efaa547f8461d4fa864eb40558cd0";
-        let snAddr = "0xa5cec2b8cda30da3f3170b4505cb44226b6c9dd2";
+        let snAddr = "0xd52114c4071b5bfbd06a657a3db538bfd559a481";
         let flag = await isUnlocked(web3, fromAddr);
         if (!flag) {
             await web3.eth.personal.unlockAccount(fromAddr, '123', 1000);
@@ -192,7 +192,7 @@ describe('Safe4Plugin SuperNode Tests', () => {
     });
 
     test("getInfo", async () => {
-        let snAddr = "0x26e701eb4369c9892bb45381bc853aa80e5aa6e8";
+        let snAddr = "0xa981b5f8d3cdb950243b970a6420e4b703a23006";
         let result = await web3.safe4.supernode.getInfo(snAddr);
         console.log(result);
         expect(result.enode.length).not.toEqual(0);
@@ -207,70 +207,70 @@ describe('Safe4Plugin SuperNode Tests', () => {
     test("getAll", async () => {
         let result = await web3.safe4.supernode.getAll();
         console.log(result);
-        expect(result.length).toBeGreaterThan(1);
+        expect(result.length).toBeGreaterThanOrEqual(4);
     });
 
     test("getTops", async () => {
         let result = await web3.safe4.supernode.getTops();
         console.log(result);
-        expect(result.length).toBeGreaterThan(4);
+        expect(result.length).toBeGreaterThanOrEqual(4);
     });
 
     test("getOfficials", async () => {
         let result = await web3.safe4.supernode.getOfficials();
         console.log(result);
-        expect(result.length).toBeGreaterThan(1);
+        expect(result.length).toBeGreaterThanOrEqual(4);
     });
 
     test("getNum", async () => {
         let result = await web3.safe4.supernode.getNum();
         console.log(result);
-        expect(result).toBeGreaterThan(4n);
+        expect(result).toBeGreaterThanOrEqual(4n);
     });
 
     test("exist", async () => {
-        let snAddr = "0xd52114c4071b5bfbd06a657a3db538bfd559a481";
+        let snAddr = "0xa981b5f8d3cdb950243b970a6420e4b703a23006";
         let result = await web3.safe4.supernode.exist(snAddr);
         console.log(result);
         expect(result).toEqual(true);
     });
 
     test("existID", async () => {
-        let result = await web3.safe4.supernode.existID(3);
+        let result = await web3.safe4.supernode.existID(6);
         console.log(result);
         expect(result).toEqual(true);
     });
 
     test("existName", async () => {
-        let name = "i-sn";
+        let name = "test-sn";
         let result = await web3.safe4.supernode.existName(name);
         console.log(result);
         expect(result).toEqual(true);
     });
 
     test("existEnode", async () => {
-        let enode = "enode://7d9cd0f84db59f040755bb7475fddd5190521db7373e6a3d8bf35b95727851821ab7e8dcb1ad3ea89f6a7d5d2e82e052b23a2a53ea5a9a86d708c7a02d2913c8@10.0.0.103:30303";
+        let enode = "enode://7d9cd0f84db59f040755bb7475fddd5190521db7373e6a3d8bf35b95727851821ab7e8dcb1ad3ea89f6a7d5d2e82e052b23a2a53ea5a9a86d708c7a02d2913c8@10.0.0.105:30303";
         let result = await web3.safe4.supernode.existEnode(enode);
         console.log(result);
         expect(result).toEqual(true);
     });
 
     test("existLockID", async () => {
-        let snAddr = "0xd52114c4071b5bfbd06a657a3db538bfd559a481";
-        let result = await web3.safe4.supernode.existLockID(snAddr, 10);
+        let snAddr = "0xa981b5f8d3cdb950243b970a6420e4b703a23006";
+        let result = await web3.safe4.supernode.existLockID(snAddr, 9);
         console.log(result);
         expect(result).toEqual(true);
     });
 
     test("isValid", async () => {
-        let snAddr = "0xd52114c4071b5bfbd06a657a3db538bfd559a481";
+        let snAddr = "0xa981b5f8d3cdb950243b970a6420e4b703a23006";
         let result = await web3.safe4.supernode.isValid(snAddr);
         console.log(result);
         expect(result).toEqual(true);
     });
 
     test("isFormal", async () => {
-        let snAddr = "0x0000000000000000000000000000000000000000";
+        let snAddr = "0xa981b5f8d3cdb950243b970a6420e4b703a23006";
         let result = await web3.safe4.supernode.isFormal(snAddr);
         console.log(result);
         expect(result).toEqual(true);
