dbf0d5050d5b9db9f7a09e7408e445dcdd4d882f
lemengbin <lemengbin@163.com>
2023-12-15 17:08:15 +0800

    fix account-unlock-validation

--- test/unit/test_util.ts
+++ test/unit/test_util.ts
@@ -2,7 +2,7 @@ import {Web3} from "web3";
 
 export async function isUnlocked(web3: Web3, address: string) {
     try {
-        await web3.eth.sign("", address);
+        await web3.eth.sign("0x01", address);
     } catch (e) {
         return false;
     }
